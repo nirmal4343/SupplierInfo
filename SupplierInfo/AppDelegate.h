147311e19e24b49e731f183489fefd7a03e3fda6
NIrmal Thakur <nirmalthakur@unknown28cfe905142b.gateway.2wire.net>
2013-09-13 20:32:41 -0400

    Initial Commit

---
+++ SupplierInfo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SupplierInfo
+//
+//  Created by NIrmal Thakur on 9/13/13.
+//  Copyright (c) 2013 NIrmal Thakur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SupplierInfo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SupplierInfo
+//
+//  Created by NIrmal Thakur on 9/13/13.
+//  Copyright (c) 2013 NIrmal Thakur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
